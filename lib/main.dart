32c11dfe028f32be8a7f5e5d09b2405a6960807b
JIhadul Islam <jihadulislam@bdcallings-Mac-mini-2.local>
2025-11-23 16:35:48 +0600

    new feature add

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quikle_rider/app.dart';
 import 'package:quikle_rider/core/services/storage_service.dart';
+import 'package:quikle_rider/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await StorageService.init();
 
   runApp(const MyApp());
